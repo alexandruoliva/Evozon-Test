38bddf8838190291c3590e045f9be8a84dbd480a
Oliva Alexandru <olivaalexandru@yahoo.com>
2020-09-05 17:51:13 +0300

    Implemeneted the 'get all' method for products

--- src/main/resources/com/evozon/dao/impl/DefaultProductDAO.java
+++ src/main/resources/com/evozon/dao/impl/DefaultProductDAO.java
@@ -1,15 +1,31 @@
 package evozon.dao.impl;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.hibernate.cfg.NotYetImplementedException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import evozon.dao.ProductDAO;
 import evozon.model.Product;
 
 import java.util.List;
 
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class DefaultProductDAO implements ProductDAO {
 
-    //The methods must be implemented
+    @Autowired
+    private SessionFactory sessionFactory;
 
     @Override
     public Product getById(int id) {
@@ -18,7 +34,16 @@ public class DefaultProductDAO implements ProductDAO {
 
     @Override
     public List<Product> getAll() {
-        throw new NotYetImplementedException();
+    	Session session = sessionFactory.getCurrentSession();
+    	CriteriaBuilder cb = session.getCriteriaBuilder();
+        CriteriaQuery < Product > cq = cb.createQuery(Product.class);
+        Root < Product > root = cq.from(Product.class);
+        cq.select(root);
+        Query query = session.createQuery(cq);
+        return query.getResultList();
+    	
+    	//TODO
+//        throw new NotYetImplementedException();
     }
 
     @Override
